a1cb2dc0d1891b2e9eb39195c21ffd05331bf835
Nuwan Fernando <fnuwan123@gmail.com>
2023-06-17 18:59:51 +0530

    Version 4

--- Async.js
+++ Async.js
@@ -1,6 +1,12 @@
-const {readFile} = require('fs')
+//const {readFile, writeFile} = require('fs')
+const {readFile, writeFile} = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-const getText = (path) =>{
+
+// This is when using  getText
+/* const getText = (path) =>{
     return new Promise((resolve, reject)=>{
         readFile(path, 'utf8', (err,data)=>{
             if(err){
@@ -15,3 +21,43 @@ const getText = (path) =>{
 getText('./read/test.txt')
     .then((result) => console.log(result))
     .catch((err)=> console.log(err)) 
+
+const start = async() => {
+    try {
+        const first = await getText('./read/test.txt');
+        const second = await getText('./read/hi.txt');
+        console.log(first, second)
+    } catch(error) {
+        console.log(error)
+    }
+    
+} */
+
+
+//This is when using util
+/* const start = async() => {
+    try {
+        const first = await readFilePromise('./read/test.txt', 'utf8');
+        const second = await readFilePromise('./read/hi.txt', 'utf8');
+        await writeFilePromise('./Promises-result.txt', 'This is awesome')
+        console.log(first, second)
+    } catch(error) {
+        console.log(error)
+    }
+    
+} */
+
+//This is when using Promisses
+const start = async() => {
+    try {
+        const first = await readFile('./read/test.txt', 'utf8');
+        const second = await readFile('./read/hi.txt', 'utf8');
+        await writeFile('./Promises-result.txt', 'This is awesome')
+        console.log(first, second)
+    } catch(error) {
+        console.log(error)
+    }
+    
+}
+
+start()
